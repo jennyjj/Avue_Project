edefc94c07b596ce9b4c81eb815693ec6f5dc3f9
vagrant <vagrant@vagrant.vm>
2017-12-09 06:14:22 +0000

    Changed slightly column name for user_id in users table.

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ class UserRoles(db.Model):
     __tablename__ = 'roles_users'
 
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.user_id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
 
